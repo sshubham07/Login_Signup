971acf85dde49caa4691d80430c9c85d2489db6f
Shubham Kumar Gupta <51732065+sshubham07@users.noreply.github.com>
2023-02-11 18:51:39 +0530

    Update info.py

--- login_signup/info.py
+++ login_signup/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'shubhamkumargupta2@gmail.com'
-EMAIL_HOST_PASSWORD = 'gofzvwfzvvsixvks'
+EMAIL_HOST_USER = 'shubhamkumarg######'
+EMAIL_HOST_PASSWORD = 'PASSWORD_FROM_GOOGLE_API'
 EMAIL_PORT = 587
